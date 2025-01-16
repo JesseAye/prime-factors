6a02dab51be9f0a8a6996f53359e35ee51bfc7f5
Jesse Ayala <jesserayala95@gmail.com>
2025-01-16 02:19:45 -0500

    Updates build task to use G++ instead of GCC Basic first iteration of calculating prime factors

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -1,4 +1,27 @@
+#include <string>
+
+std::string calculate_prime_factors(unsigned int number)
+{
+    std::string result = "1";
+    for (unsigned int i = 2; i <= number; i++)
+    {
+        unsigned int remainder = number % i;
+
+        if (remainder == 0)
+        {
+            std::string remainder_str = (", " + std::to_string(i));
+            result += remainder_str;
+        }
+    }
+    
+    result += "\n";
+    return result;
+}
+
 int main(int argc, char *argv[])
 {
+    unsigned int number = 21;
+    std::string primes = calculate_prime_factors(number);
+    printf(primes.c_str());
     return 0;
 }
