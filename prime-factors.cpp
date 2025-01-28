031d29a447208d82808df65f834b1a33063a3b47
Jesse Ayala <jesserayala95@gmail.com>
2025-01-28 01:21:00 -0500

    Use easier typedef uint instead of unsigned int

--- prime-factors.cpp
+++ prime-factors.cpp
@@ -2,13 +2,13 @@
 #include <vector>
 #include <map>
 
-std::vector<unsigned int> calculate_factors(unsigned int number)
+std::vector<uint> calculate_factors(uint number)
 {
-    std::vector<unsigned int> results;
+    std::vector<uint> results;
     results.push_back(1);
-    for (unsigned int i = 2; i <= (number / 2); i++)
+    for (uint i = 2; i <= (number / 2); i++)
     {
-        unsigned int remainder = number % i;
+        uint remainder = number % i;
 
         if (remainder == 0)
         {
@@ -20,13 +20,13 @@ std::vector<unsigned int> calculate_factors(unsigned int number)
     return results;
 }
 
-std::map<unsigned int, unsigned int> calculate_prime_factors(unsigned int number)
+std::map<uint, uint> calculate_prime_factors(uint number)
 {
-    std::map<unsigned int, unsigned int> results;
-    unsigned int curr_factor = 2;
+    std::map<uint, uint> results;
+    uint curr_factor = 2;
     while (curr_factor <= number && curr_factor > 1)
     {
-        unsigned int remainder = number % curr_factor;
+        uint remainder = number % curr_factor;
 
         if (remainder == 0)
         {
@@ -44,10 +44,10 @@ std::map<unsigned int, unsigned int> calculate_prime_factors(unsigned int number
 
 int main(int argc, char *argv[])
 {
-    unsigned int number = 25;
-    std::map<unsigned int, unsigned int> prime_factors = calculate_prime_factors(number);
+    uint number = 25;
+    std::map<uint, uint> prime_factors = calculate_prime_factors(number);
     
-    std::map<unsigned int, unsigned int>::iterator it = prime_factors.begin();
+    std::map<uint, uint>::iterator it = prime_factors.begin();
     printf("%lu^%lu", it->first, it->second);
     it++;
     while(it != prime_factors.end())
@@ -57,9 +57,9 @@ int main(int argc, char *argv[])
     }
     printf("\n");
 
-    std::vector<unsigned int> primes = calculate_factors(number);
+    std::vector<uint> primes = calculate_factors(number);
     printf("%lu", primes.at(0));
-    for (std::vector<unsigned int>::iterator it = primes.begin() + 1; it != primes.end(); it++)
+    for (std::vector<uint>::iterator it = primes.begin() + 1; it != primes.end(); it++)
     {
         printf(", %lu", *it);
     }
